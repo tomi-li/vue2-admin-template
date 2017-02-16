782d36f6bceeee9f287a8b0a8e9f8e0d410fa309
tomi <tomi@ccfocusone.com>
2017-02-16 15:49:24 +0800

    [trivial] modify webpack

--- build/build.js
+++ build/build.js
@@ -26,10 +26,11 @@ webpack(webpackConfig, function (err, stats) {
   spinner.stop()
   if (err) throw err
   process.stdout.write(stats.toString({
-    colors: true,
-    modules: false,
-    children: false,
-    chunks: false,
-    chunkModules: false
-  }) + '\n')
+      colors: true,
+      modules: false,
+      children: false,
+      chunks: false,
+      chunkModules: false,
+      profile: true
+    }) + '\n')
 })
